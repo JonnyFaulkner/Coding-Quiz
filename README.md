049516c02ed651ecefefcdd68ff0435164b9f9b7
Noctus <jonnyfaulkner21@gmail.com>
2021-06-06 23:53:31 -0500

    added Questions

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Coding-Quiz
+
+## Purpose
+A application that quizes the user on basic coding knowledge.
+
+## Built With
+* HTML
+* JavaScript
+
+## Website
+
+
+## Screenshots
+
+
+## Contribution
+Made by Jonathan Faulkner
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,7 @@
 var score = 0;
+var timer = 60;
+var currentQuestion = 0;
+
 var quiz = document.getElementById("quiz")
 var quizStart = document.createElement("div")
 var timerEl = document.getElementById("timer")
@@ -30,8 +33,98 @@ var quizStartScreen = function() {
         startHeader.remove();
         startDescription.remove();
         startButton.remove();
+
+        var questions = [
+            {
+                q: "DOM stands for ____.",
+                a1: "Dollar Options Menu" ,
+                a2: "Definitive Operating Menu",
+                a3: "Document Object Model",
+                a4: "Direct Options Model",
+                cA: "Document Object Model"
+            },
+            {
+                q: "GitHub is a website that hosts ____.",
+                a1: "Repositories",
+                a2: "Parties",
+                a3: "Video Calls",
+                a4: "Metal Concerts",
+                cA: "Repositories"
+            },
+            {
+                q: "CSS stands for",
+                a1: "Calculated Style Sheet",
+                a2: "Calendar Support System",
+                a3: "Car Seat Strap",
+                a4: "Casscading Style Sheet",
+                cA: "Casscading Style Sheet"
+            }
+        ];
+        var questionContainer = document.createElement("div");
+        var questionsStart = function(){
+                
+            for (var i = 0; i < questions.length; i++) {
+                currentQuestion = questions[i];
+                var question = document.createElement("p")
+               
+                question.textContent = currentQuestion.q;
+                questionContainer.appendChild(question)
+                
+                var answer1 = document.createElement("button")
+                answer1.textContent = currentQuestion.a1
+                questionContainer.appendChild(answer1)
+                
+                var answer2 = document.createElement("button")
+                answer2.textContent = currentQuestion.a2
+                questionContainer.appendChild(answer2)
+                
+                var answer3 = document.createElement("button")
+                answer3.textContent = currentQuestion.a3
+                questionContainer.appendChild(answer3)
+                
+                var answer4 = document.createElement("button")
+                answer4.textContent = currentQuestion.a4
+                questionContainer.appendChild(answer4)
+
+                quiz.appendChild(questionContainer)
+                
+                var clickEvent = function() {
+                answer1.addEventListener("click", function(){
+                    if (answer1.textContent === currentQuestion.cA) {
+                        console.log(answer1.textContent)
+                        score + 5
+                        currentQuestion++
+                    }
+                    
+                })
+
+                answer2.addEventListener("click", function(){
+                    if (answer2.textContent === currentQuestion.cA) {
+                        console.log(answer2.textContent)
+                        score + 5
+                    }
+                })
+
+                answer3.addEventListener("click", function(){
+                    if (answer3.textContent === currentQuestion.cA){
+                        console.log(answer3.textContent)
+                        score + 5
+                        currentQuestion++
+                    }
+                })
+
+                answer4.addEventListener("click", function(){
+                    if (answer4.textContent === currentQuestion.cA){
+                        console.log(answer4.textContent)
+                        score + 5
+                    }
+                })
+                }
+                clickEvent()   
+                quiz.appendChild(questionContainer)
+            }
+        }
         function timerStart() {
-            var timer = 60;
             var time = setInterval(function () {
                 if (timer > 0) {
                     timerEl.textContent = timer;
@@ -42,14 +135,15 @@ var quizStartScreen = function() {
                     window.alert("You have failed!")
                     timerEl.textContent = ""
                     quizStartScreen()
+                    questionContainer.remove()
+                    timer = 60;
                 }
             }, 1000);
         };
         
         timerStart()
 
-        var question1 = document.createElement("div")
-        
+        questionsStart()
     };
 
     startButton.addEventListener("click", startQuiz)
